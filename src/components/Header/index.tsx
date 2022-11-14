c78d71f76ab203354ab4a97809b4185565d68cf1
unknown <elizeucosta21@gmail.com>
2022-11-14 13:36:59 -0300

    feat: criacao do cabecalho do site

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { StyledHeader } from "./style";
+import Logo from "../../assets/logo.png";
+import { useContext } from "react";
+import { UserContext } from "../../context/UserContext";
+
+export default function Header() {
+  const { logout, user } = useContext(UserContext);
+  return (
+    <StyledHeader>
+      <h3>Seja bem-vindo, {user.name}</h3>
+      <img src={Logo} alt='logomarca da aplicação movies collection' />
+      <button onClick={() => logout()}>Logout</button>
+    </StyledHeader>
+  );
+}
